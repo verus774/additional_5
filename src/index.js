3e63bcdd88357719d4396dbd461fc32a30752990
Sergey Ladutko <verus774@mail.ru>
2018-02-27 20:54:31 +0300

    feature: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let openBracket = bracketsConfig[j][0];
+            let closeBracket = bracketsConfig[j][1];
+
+            if (char === openBracket) counter++;
+            else if (char === closeBracket) counter--;
+            if (counter < 0) return false;
+        }
+    }
+
+    return counter === 0;
+};
