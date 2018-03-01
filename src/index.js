27527ee3715010038a7ed0649a1cad59de9ff914
Sergey Ladutko <verus774@mail.ru>
2018-03-01 22:04:00 +0300

    fix: use stack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-    let counter = 0;
+    let stack = [];
+    let openBrackets = [];
+    let closeBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+    }
 
     for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            let openBracket = bracketsConfig[j][0];
-            let closeBracket = bracketsConfig[j][1];
+        const char = str[i];
 
-            if (char === openBracket) counter++;
-            if (char === closeBracket) counter--;
-            if (counter < 0) return false;
+        if (openBrackets.indexOf(char) >= 0) {
+            stack.push(char);
+        }
+
+        if (closeBrackets.indexOf(char) >= 0) {
+            const stackLength = stack.length;
+            const topEl = stack[stackLength - 1];
+
+            if (stackLength > 0 && topEl === openBrackets[closeBrackets.indexOf(char)]) {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
     }
 
-    return counter === 0;
+    return stack.length === 0;
 };
